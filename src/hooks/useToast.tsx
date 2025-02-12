9cb6588e3600425edd93e1c0fd823a3209c1283d
Madhav <madhavsingh.relish@gmail.com>
2025-02-12 20:39:22 +0530

    Created useToast hook with context

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -1,13 +1,38 @@
-import { useState } from "react"
+import React from "react";
+import { createContext, useContext, useState } from "react";
+import { toastContextType, toastType } from "../types/toastTypes";
+import ToastContainer from "../components/toast";
 
+export const ToastContext = createContext<toastContextType | undefined>(undefined);
 
-type Props = {
-    message?: string
-}
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) {
+    throw new Error("Toast must be in <ToastProvide/>!");
+  }
+  return context;
+};
 
-const useToast = ({message}: Props) => {
-    const [toast, setToast] = useState(message || "")
-  return {toast}
-}
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toasts, setToasts] = useState<toastType[]>([]);
 
-export default useToast
+  const showToast = (
+    message: string,
+    type: toastType["type"] = "default",
+    duration: number = 3000
+  ) => {
+    const id = Date.now();
+    setToasts((prev) => [...prev, { id, message, type, duration }]);
+
+    setTimeout(() => {
+      setToasts((prev) => prev.filter((toast) => toast.id !== id));
+    }, duration);
+  };
+
+  return (
+    <ToastContext.Provider value={{ showToast, toasts }}>
+      {children}
+      <ToastContainer/>
+    </ToastContext.Provider>
+  );
+};
